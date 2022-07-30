fb4cb35730b0237fc96b70ba6d8b55f05fa2229e
MonkeyDDeveloper <fraydeveloper@gmail.com>
2022-07-29 23:40:54 -0500

    changing mysql server

--- mysql/connection.js
+++ mysql/connection.js
@@ -1,12 +1,7 @@
 const path = require('path');
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = require(path.join(__dirname, '..', 'config', 'config.js'));
+const {DB_URL} = require(path.join(__dirname, '..', 'config', 'config.js'));
 const mysql = require('mysql2');
-const contactsConnection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME
-})
+const contactsConnection = mysql.createConnection(DB_URL)
 
 function connectDatabase () {
     return new Promise( (res, rej) => {
